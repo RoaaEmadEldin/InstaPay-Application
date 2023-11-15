e78a4bbd342dba087daf07b883c9f4f517eef322
Shrouk Heshmat <102330882+shroukheshmat1@users.noreply.github.com>
2023-11-15 22:34:56 +0200

    InstaPayDatabaseShroukV1.java
    add input to the DB manually for using it in the transferring from an account to another

--- Database/InstaPayDatabase.java
+++ Database/InstaPayDatabase.java
@@ -1,12 +1,19 @@
 package Database;
 
+import Account.BankAccountUser;
 import Account.UserAccount;
+import Account.WalletAccountUser;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.lang.String;
+import java.util.Map;
 
 public class InstaPayDatabase {
-    private static List<UserAccount> accounts = new ArrayList<>();
+    private static List<UserAccount> accounts = new ArrayList<>(Collections.singleton(new BankAccountUser("RoaaEmad", "01014425536", "1234 5678 9101 1121", "Roaa123#")));
+
+
 
     public static void add(UserAccount user) {
         accounts.add(user);
